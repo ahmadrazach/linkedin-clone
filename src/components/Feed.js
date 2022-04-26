8f2a95a6f63a6b717637723eeb612e26b5f00889
Ahmad Raza <ahmadrazach26@gmail.com>
2022-04-27 01:48:37 +0500

    Done with making the header background change and done with adding Input Option in Feed and making INputOption code and styling

--- src/components/Feed.js
+++ src/components/Feed.js
@@ -1,6 +1,11 @@
 import React from 'react';
 import './Feed.css';
 import CreateIcon from '@material-ui/icons/Create';
+import InputOption from './InputOption';
+import ImageIcon from '@material-ui/icons/Image';
+import SubscriptionsIcon from '@material-ui/icons/Subscriptions'
+import EventNoteIcon from "@material-ui/icons/EventNote";
+import CalendarViewDayIcon from '@material-ui/icons/CalendarViewDay';
 
 const Feed = () => {
   return (
@@ -12,6 +17,13 @@ const Feed = () => {
                     <input type="text"/>
                     <button type='submit'>Send</button>
                 </form>
+            </div>
+
+            <div className='feed__inputOptions'>
+                <InputOption Icon={ImageIcon} title='Photo' color="#7005F9"/>
+                <InputOption Icon={SubscriptionsIcon} title='Video' color="#E7A33E"/>
+                <InputOption Icon={EventNoteIcon} title='Event' color="#C0CBCD"/>
+                <InputOption Icon={CalendarViewDayIcon} title='Write Article' color="#7FC1SE"/>
             </div>
         </div>
     </div>
